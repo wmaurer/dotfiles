2e239636c64a18745c4ddba8546ffd3151f8da1f
Wayne Maurer <wmaurer@users.noreply.github.com>
2017-09-25 09:49:25 +0200

    Update Microsoft.PowerShell_profile.ps1

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,8 +1,9 @@
 Set-PSReadlineOption -BellStyle None
 
 Set-Alias np "C:\Program Files (x86)\Notepad++\notepad++.exe"
-Set-Alias edit-profile "C:\Program Files (x86)\Notepad++\notepad++.exe" $env:USERPROFILE\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
 Set-Alias vi "C:\Users\wayne\Get-ToolsLocation\neovim\Neovim\bin\nvim.exe"
+
+function edit-profile { Start-Process "C:\Program Files (x86)\Notepad++\notepad++.exe" $env:USERPROFILE\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1 }
 
 # Chocolatey profile
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
