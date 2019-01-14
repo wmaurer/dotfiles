5f2f54668ad0522bf130b3520d4689d86c5f72a4
Wayne Maurer <wayne@waynemaurer.net>
2019-01-14 22:29:16 +0100

    typescript snippets

--- vscode/copy-from-machine.ps1
+++ vscode/copy-from-machine.ps1
@@ -1,2 +1,3 @@
 Copy-Item -Path $home\AppData\Roaming\Code\User\settings.json .
 Copy-Item -Path $home\AppData\Roaming\Code\User\keybindings.json .
+Copy-Item -Path $home\AppData\Roaming\Code\User\snippets .\snippets -Recurse
