c2a02c72990873a4776951a1af23d327a7be5997
Wayne Maurer <2899448+wmaurer@users.noreply.github.com>
2024-03-04 23:26:17 +0100

    x

---
+++ lvim/config.lua
@@ -0,0 +1,27 @@
+-- Read the docs: https://www.lunarvim.org/docs/configuration
+-- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
+-- Forum: https://www.reddit.com/r/lunarvim/
+-- Discord: https://discord.com/invite/Xb9B4Ny
+
+lvim.format_on_save.enabled = true
+
+local formatters = require "lvim.lsp.null-ls.formatters"
+formatters.setup {
+  {
+    command = "prettier",
+    filetypes = { "typescript", "typescriptreact" },
+  },
+}
+
+-- https://www.reddit.com/r/neovim/comments/11xn9w1/comment/jd72vtt/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
+require 'lspconfig'.eslint.setup({
+  settings = {
+    packageManager = 'yarn'
+  },
+  on_attach = function(client, bufnr)
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      buffer = bufnr,
+      command = "EslintFixAll",
+    })
+  end,
+})
