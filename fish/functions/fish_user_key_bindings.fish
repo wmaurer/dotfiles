7dde69935ea0874cad4eed6ef77385e72e671f1f
Wayne Maurer <2899448+wmaurer@users.noreply.github.com>
2024-06-11 14:32:03 +0200

    x

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -1,7 +1,8 @@
 function fish_user_key_bindings
     for mode in insert default visual
         bind -M $mode \cf forward-char # bind ctrl-f to autocomplete
-	bind -M $mode \cd delete-char # stop fish exiting on ctrl-d
-	bind -M $mode \cd\cd\cd delete-or-exit # extit on third one
+        bind -M $mode \cl forward-word 
+	    bind -M $mode \cd delete-char # stop fish exiting on ctrl-d
+	    bind -M $mode \cd\cd\cd delete-or-exit # extit on third one
     end
 end
