7f368e10c4d5d17c3e7e8c2e5428d16af87556c4
Wayne Maurer <mrwe1@office.begasoft.ch>
2026-03-09 21:57:37 +0100

    Migrate nvim config to AstroNvim v5

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,49 +1,27 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
+-- This file simply bootstraps the installation of Lazy.nvim and then calls other files for execution
+-- This file doesn't necessarily need to be touched, BE CAUTIOUS editing this file and proceed at your own risk.
+local lazypath = vim.env.LAZY or vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+
+if not (vim.env.LAZY or (vim.uv or vim.loop).fs_stat(lazypath)) then
+  -- stylua: ignore
+  local result = vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+  if vim.v.shell_error ~= 0 then
+    -- stylua: ignore
+    vim.api.nvim_echo({ { ("Error cloning lazy.nvim:\n%s\n"):format(result), "ErrorMsg" }, { "Press any key to exit...", "MoreMsg" } }, true, {})
+    vim.fn.getchar()
+    vim.cmd.quit()
+  end
 end
+
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({
-    "tpope/vim-surround",
-    "tpope/vim-repeat",
-    "tpope/vim-unimpaired",
-    "unblevable/quick-scope",
-    "smoka7/hop.nvim"
-})
-
-require'hop'.setup()
-
-
-vim.o.clipboard = "unnamedplus"
-vim.o.ignorecase = true
-vim.o.smartcase = true
-
-vim.api.nvim_set_keymap('n', '0', '^', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '^', '0', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('x', 'p', '"_dP', { noremap = true, silent = true })
-
-if vim.g.vscode then
-    vim.keymap.set('n', '<space>', ':call VSCodeNotify("vspacecode.space")<CR>')
-    local function set_hl(group, tbl)
-        vim.api.nvim_set_hl(0, group, type(tbl) == 'table' and tbl or { fg = tbl })
-    end
-
-    set_hl('HopNextKey', { fg = '#ffffff', bg = '#ff007c', bold = true, italic = false })
-    set_hl('HopNextKey1', { fg = '#ffffff', bg = '#f76707', bold = true, italic = false })
-    set_hl('HopNextKey2', { fg = '#ffffff', bg = '#e67700', bold = true, italic = false })
-    set_hl('HopUnmatched', { fg = '#666666', sp = '#666666' })
-else
-    vim.o.tabstop = 4
-    vim.o.shiftwidth = 4
-    vim.o.smarttab = true
-    vim.o.expandtab = true
+-- validate that lazy is available
+if not pcall(require, "lazy") then
+  -- stylua: ignore
+  vim.api.nvim_echo({ { ("Unable to load lazy from: %s\n"):format(lazypath), "ErrorMsg" }, { "Press any key to exit...", "MoreMsg" } }, true, {})
+  vim.fn.getchar()
+  vim.cmd.quit()
 end
 
+require "lazy_setup"
+require "polish"
